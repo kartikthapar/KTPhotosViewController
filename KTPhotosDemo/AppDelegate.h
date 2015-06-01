bd365d4746242348afcd48d80ceaaadf7d37fab6
Kartik Thapar <Kartik.Thapar@me.com>
2015-06-01 10:03:14 -0400

    - KTPhotos project, README

---
+++ KTPhotosDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KTPhotos
+//
+//  Created by Kartik Thapar on 6/1/15.
+//  Copyright (c) 2015 Kartik Thapar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KTPhotosDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KTPhotos
+//
+//  Created by Kartik Thapar on 6/1/15.
+//  Copyright (c) 2015 Kartik Thapar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
