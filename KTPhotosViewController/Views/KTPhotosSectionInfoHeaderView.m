9fe34a685c086ff13f957e7c10afb009f12c7aa9
Kartik Thapar <Kartik.Thapar@me.com>
2015-06-05 03:44:26 -0400

    - adding a title label

--- KTPhotosViewController/Views/KTPhotosSectionInfoHeaderView.m
+++ KTPhotosViewController/Views/KTPhotosSectionInfoHeaderView.m
@@ -10,7 +10,10 @@
 
 @interface KTPhotosSectionInfoHeaderView ()
 
+@property (nonatomic, strong) UILabel *titleLabel;
+
 - (void)kt_configureSectionInfoHeaderView;
+- (void)kt_configureConstraintsForTitleLabel;
 
 @end
 
@@ -28,6 +31,26 @@
 - (void)kt_configureSectionInfoHeaderView
 {
     self.accessibilityLabel = KTPhotosSectionInfoHeaderViewAccessibilityLabel;
+    
+    self.titleLabel = [[UILabel alloc] init];
+    self.titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self addSubview:self.titleLabel];
+    
+    [self kt_configureConstraintsForTitleLabel];
+}
+
+#pragma mark - Constraints
+
+- (void)kt_configureConstraintsForTitleLabel
+{
+    NSDictionary *metrics = @{@"leftMargin":@(15),
+                              @"topMargin":@(5)};
+    NSDictionary *views = @{@"titleLabel": self.titleLabel};
+    NSArray *leftMarginConstraint = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-leftMargin-[titleLabel]" options:0 metrics:metrics views:views];
+    NSArray *topMarginConstraint = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-topMargin-[titleLabel]" options:0 metrics:metrics views:views];
+
+    [self addConstraints:leftMarginConstraint];
+    [self addConstraints:topMarginConstraint];
 }
 
 #pragma mark - Info
